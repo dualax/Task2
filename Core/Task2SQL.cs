c1699153bdbb0a0fadfa62f4d982ab99d0fa604a
dualax <158518020+dualax@users.noreply.github.com>
2025-04-15 10:33:13 +0300

    Add files via upload

---
+++ Core/Task2SQL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task2.Modwl;
+
+namespace Task2.Core
+{
+    public static class Task2SQL
+    {
+        public static Task2bdEntities DB { get; set; }
+    }
+}
